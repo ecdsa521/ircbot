d3af3770d71ed54eec7901d3fbfb65c744a9c693
ecdsa521 <ecdsa521@gmail.com>
2017-03-02 16:48:22 +0100

    First somewhat working ... somewhat... version

--- init.go
+++ init.go
@@ -2,18 +2,37 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"time"
 
 	"github.com/ecdsa521/ircbot/lib"
 )
 
 func main() {
 
+	var ch = make(chan string)
 	var irc = ircbot.IrcConn{
-		Nick:  "test",
-		Ident: "test",
-		Host:  "irc.freenode.net",
+		Nick:  "moobot",
+		Ident: "moobot",
+		Host:  "127.0.0.1",
+		Port:  "6667",
 	}
 
 	ircbot.Connect(&irc)
-	fmt.Printf("%v\n", irc)
+	ircbot.JoinChannel(&irc, "#")
+	irc.WaitGroup.Add(2)
+
+	go func() {
+		go ircbot.Poll(&irc, ch)
+		for line := range ch {
+			if matches, err := regexp.MatchString("INVITE", line); err == nil && matches {
+				ircbot.JoinChannel(&irc, "#")
+			}
+			fmt.Println(line)
+		}
+		defer irc.WaitGroup.Done()
+	}()
+
+	time.Sleep(1 * time.Hour)
+	irc.WaitGroup.Wait()
 }
--- lib/bot.go
+++ lib/bot.go
@@ -1,7 +1,11 @@
 package ircbot
 
 import (
+	"bufio"
 	"fmt"
+	"net"
+	"sync"
+	"time"
 )
 
 //IrcEvent ...
@@ -13,20 +17,68 @@ type IrcEvent struct {
 
 //IrcConn ...
 type IrcConn struct {
-	Host   string
-	Port   int
-	UseSsl bool
-	Ident  string
-	Name   string
-	Nick   string
+	Host      string
+	Port      string
+	UseSsl    bool
+	Ident     string
+	Name      string
+	Nick      string
+	Socket    net.Conn
+	Buffer    *bufio.ReadWriter
+	Reader    *bufio.Reader
+	Writer    *bufio.Writer
+	WaitGroup *sync.WaitGroup
 }
 
 func init() {
-	fmt.Println("init ircbot")
+
 }
 
 //Connect ...
 func Connect(irc *IrcConn) {
-	fmt.Printf("connecting %v\n", irc)
+	var err error
+	fmt.Printf("Connecting to %s:%s\n", irc.Host, irc.Port)
+	if irc.Socket, err = net.Dial("tcp", irc.Host+":"+irc.Port); err == nil {
+		fmt.Printf("Connected to %s:%s\n", irc.Host, irc.Port)
+	} else {
+		fmt.Printf("Error connecting: %v\n", err)
+	}
+
+	irc.Reader = bufio.NewReader(irc.Socket)
+	irc.Writer = bufio.NewWriter(irc.Socket)
+	//irc.Writer = bufio.NewWriter(&irc.Socket)
+	irc.Buffer = bufio.NewReadWriter(irc.Reader, irc.Writer)
+	time.Sleep(1 * time.Second)
+	Write(irc, fmt.Sprintf("USER %s %s %s :%s", irc.Ident, irc.Ident, irc.Ident, irc.Ident))
+	Write(irc, fmt.Sprintf("NICK %s", irc.Nick))
+}
+
+//Write to irc Socket
+func Write(irc *IrcConn, message string) {
+	fmt.Fprintf(irc.Socket, "%s\n", message)
+}
+
+//JoinChannel .. joins a channel
+func JoinChannel(irc *IrcConn, channel string) {
+	Write(irc, "JOIN "+channel)
+}
+
+//Poll ..
+func Poll(irc *IrcConn, ch chan<- string) {
+
+	var err error
+	var msg string
+	for {
+		fmt.Println("polling")
+		msg, err = irc.Buffer.ReadString('\n')
+		if err != nil {
+			break
+		}
+		ch <- fmt.Sprintf("%s\n", msg)
+
+	}
+	defer close(ch)
+	fmt.Println("Out of loop")
+	defer irc.WaitGroup.Done()
 
 }
